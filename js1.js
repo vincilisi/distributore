a4c2185d1ee33b4993a0c3aa78c7bd2b93921782
vincilisi <96939594+vincilisi@users.noreply.github.com>
2024-07-22 09:35:34 +0200

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,11 @@
+let distributore= prompt("Inserisci numero della bevanda");
+console.log(distributore);
+if (distributore == 1){
+    console.log("E' stata selezionata l'acqua");
+} else if(distributore == 2){
+    console.log("E’ stata selezionata coca cola");
+} else if(distributore == 3){
+    console.log("E’ stata selezionata birra")
+} else{
+    console.log("Inserisci numero della bevanda");
+}
